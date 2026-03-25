c81cb723aee114dd2be0a2159f6fd403818a1917
naur-Io <ruanlucenaramos@gmail.com>
2026-03-24 22:51:30 -0300

    feat: creating greating model and controller with endpoint greeting with query params

---
+++ src/main/java/com/example/demo/controllers/GreetingController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    //http://localhost:8080/greeting?name=naur
+    @RequestMapping("/greeting")
+    public Greeting greeting(
+            @RequestParam(value = "name", defaultValue = "hello")
+            String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template,name));
+    }
+}
---
+++ src/main/java/com/example/demo/model/Greeting.java
@@ -0,0 +1,5 @@
+package com.example.demo.model;
+
+public record Greeting(long id, String content){
+
+}
